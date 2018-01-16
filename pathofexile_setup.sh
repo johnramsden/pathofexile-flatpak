d7e2e2de6c557af0d1f6474e6ad782d79f1e3a2b
John Ramsden <johnramsden@users.noreply.github.com>
2018-01-16 00:01:26 -0800

    Move wine install to script

--- pathofexile_setup.sh
+++ pathofexile_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 export WINEPREFIX="${HOME}/.local/share/pathofexile"
 export WINEDEBUG=-all
@@ -9,30 +9,27 @@ POE_DOWNLOAD_URL='https://www.pathofexile.com/downloads/PathOfExileInstaller.exe
 
 WINE_CMD="/app/bin/wine"
 
-# Set video memory
-if [ -f "/var/log/Xorg.0.log" ]; then
-  VIDEO_MEMORY=$(($(sed -rn 's/.*memory: ([0-9]*).*kbytes/\1/gpI' /var/log/Xorg.0.log) / 1024))
-  if [ ${VIDEO_MEMORY} -gt 2048 ]; then
-    VIDEO_MEMORY_SETTING=2048
-  else
-    VIDEO_MEMORY_SETTING=${VIDEO_MEMORY}
-  fi
-  echo "Setting video memory to ${VIDEO_MEMORY_SETTING}"
-  winetricks --unattended videomemorysize=${VIDEO_MEMORY_SETTING}
-fi
-
-winetricks --unattended vd=1920x1080
-
 if grep -q 'Software\\\\GrindingGearGames\\\\Path of Exile' ${WINEPREFIX}/user.reg; then
   echo "Path of Exile installed, running..."
   ${WINE_CMD} "${WINEPREFIX}/drive_c/Program Files/Grinding Gear Games/Path of Exile/PathOfExile.exe" \
     dbox  -no-dwrite -noasync
 else
-  echo "Path of Exile not installed."
+  echo "Path of Exile not installed, running first run settings."
+  
+  winetricks --unattended win7 csmt=on vd=1920x1080 glsl=disabled
+
+  echo "Installing first run wine requirements."
+
+  winetricks --unattended directx9 usp10 msls31 corefonts tahoma
+
   if [ ! -f "${POE_SETUP}" ]; then
     echo "Downloading Path of Exile installer."
     wget --output-document="${WINEPREFIX}/${POE_INSTALLER_NAME}" ${POE_DOWNLOAD_URL}
   fi
   echo "Running Path of Exile installer."
   "${WINE_CMD}" "${POE_SETUP}"
+
+  echo "Running Path of Exile"
+  ${WINE_CMD} "${WINEPREFIX}/drive_c/Program Files/Grinding Gear Games/Path of Exile/PathOfExile.exe" \
+    dbox  -no-dwrite -noasync
 fi
