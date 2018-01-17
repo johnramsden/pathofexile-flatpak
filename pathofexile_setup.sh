962fc62fce5562b9b9b029e9febb38cc52b78054
John Ramsden <johnramsden@users.noreply.github.com>
2018-01-17 15:59:18 -0800

    Update start script to set proper VideoMemory

--- pathofexile_setup.sh
+++ pathofexile_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export WINEPREFIX="${HOME}/.local/share/pathofexile"
 export WINEDEBUG=-all
@@ -16,15 +16,37 @@ if grep -q 'Software\\\\GrindingGearGames\\\\Path of Exile' ${WINEPREFIX}/user.r
 else
   echo "Path of Exile not installed, Installing first run wine requirements."
 
-  winetricks --unattended directx9 usp10 msls31 corefonts tahoma
+  winetricks --unattended directx9 usp10 msls31 corefonts tahoma win7
 
   echo "Running first run settings."
   
-  winetricks --unattended win7 csmt=on vd=1920x1080 glsl=disabled
+  winetricks --unattended csmt=on vd=1920x1080 glsl=disabled
+
+  # Set video memory 
+  if [ -f "/var/log/Xorg.0.log" ]; then 
+    VIDEO_MEMORY=$(($(sed -rn 's/.*memory: ([0-9]*).*kbytes/\1/gpI' /var/log/Xorg.0.log) / 1024))
+
+    echo "Setting video memory to ${VIDEO_MEMORY}" 
+    tmpfile=$(mktemp VideoMemory.XXXXX.reg)
+
+    cat <<EOF > "${tmpfile}"
+Windows Registry Editor Version 5.00
+
+[HKEY_CURRENT_USER\\Software\\Wine\\Direct3D]
+"VideoMemorySize"="${VIDEO_MEMORY}"
+
+EOF
+
+     echo "Setting registry key:"
+     cat ${tmpfile}
+
+     wine regedit ${tmpfile}
+     rm ${tmpfile}
+  fi 
 
   if [ ! -f "${POE_SETUP}" ]; then
     echo "Downloading Path of Exile installer."
-    wget --output-document="${WINEPREFIX}/${POE_INSTALLER_NAME}" ${POE_DOWNLOAD_URL}
+    wget --output-document="${POE_SETUP}" ${POE_DOWNLOAD_URL}
   fi
   echo "Running Path of Exile installer."
   "${WINE_CMD}" "${POE_SETUP}"
