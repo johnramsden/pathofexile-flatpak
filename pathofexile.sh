97346f572af856df4bec43450f17200eb02262ef
John Ramsden <johnramsden@riseup.net>
2018-03-26 17:47:49 -0700

    Modularize startup, always run vmem check

--- pathofexile.sh
+++ pathofexile.sh
@@ -11,26 +11,15 @@ WINE_RESOLUTION="${WINE_RESOLUTION:-1920x1080}"
 
 WINE_CMD="/app/bin/wine"
 
+XORG_LOG="/var/log/Xorg.0.log"
+
 echo "Path of Exile Unofficial Flatpak"
 echo
 
-echo "Setting resolution to ${WINE_RESOLUTION}, if changed from default may need restart to take effect"
-winetricks --unattended vd=${WINE_RESOLUTION}
-
-if grep -q 'Software\\\\GrindingGearGames\\\\Path of Exile' ${WINEPREFIX}/user.reg; then
-  echo "Path of Exile installed, running..."
-  ${WINE_CMD} "${WINEPREFIX}/drive_c/Program Files/Grinding Gear Games/Path of Exile/PathOfExile.exe" \
-    dbox  -no-dwrite -noasync
-else
-  echo "Path of Exile not installed, Installing first run wine requirements."
-  winetricks --unattended directx9 usp10 msls31 corefonts tahoma win7
-
-  echo "Running first run settings."
-  winetricks --unattended csmt=on glsl=disabled
-
-  # Set video memory 
-  if [ -f "/var/log/Xorg.0.log" ]; then 
-    VIDEO_MEMORY=$(($(sed -rn 's/.*memory: ([0-9]*).*kbytes/\1/gpI' /var/log/Xorg.0.log) / 1024))
+# Set video memory by checking xorg
+set_video_memory(){
+  if [ -f "${XORG_LOG}" ]; then 
+    VIDEO_MEMORY=$(($(sed -rn 's/.*memory: ([0-9]*).*kbytes/\1/gpI' ${XORG_LOG}) / 1024))
 
     echo "Setting video memory to ${VIDEO_MEMORY}" 
     tmpfile=$(mktemp VideoMemory.XXXXX.reg)
@@ -48,7 +37,19 @@ EOF
 
      wine regedit ${tmpfile}
      rm ${tmpfile}
+  else
+    echo "Unable to read Xorg logs from ${XORG_LOG}."
+    echo "Leaving video card memory at default settings."
   fi 
+}
+
+# Run only if POE isn't installed
+first_run(){
+  echo "Path of Exile not installed, Installing first run wine requirements."
+  winetricks --unattended directx9 usp10 msls31 corefonts tahoma win7
+
+  echo "Running first run settings."
+  winetricks --unattended csmt=on glsl=disabled
 
   if [ ! -f "${POE_SETUP}" ]; then
     echo "Downloading Path of Exile installer."
@@ -56,8 +57,22 @@ EOF
   fi
   echo "Running Path of Exile installer."
   "${WINE_CMD}" "${POE_SETUP}"
+}
 
-  echo "Running Path of Exile"
+# Main function
+startup(){
+  echo "Setting resolution to ${WINE_RESOLUTION}, if changed from default may need restart to take effect"
+  winetricks --unattended vd=${WINE_RESOLUTION}
+
+  set_video_memory
+
+  if ! grep -q 'Software\\\\GrindingGearGames\\\\Path of Exile' ${WINEPREFIX}/user.reg; then
+    first_run 
+  fi
+
+  echo "Starting Path of Exile..."
   ${WINE_CMD} "${WINEPREFIX}/drive_c/Program Files/Grinding Gear Games/Path of Exile/PathOfExile.exe" \
     dbox  -no-dwrite -noasync
-fi
+}
+
+startup
